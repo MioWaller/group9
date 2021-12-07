1f33035f6e92f35d4e80467fcd7eb84003290a7c
Mio Waller <38918176+MioWaller@users.noreply.github.com>
2021-12-06 23:29:09 -0600

    Sum

--- src/test/java/group9/group9/TableControllerTests.java
+++ src/test/java/group9/group9/TableControllerTests.java
@@ -1,5 +1,5 @@
 package group9.group9;
-
+/*
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.hamcrest.Matchers.containsString;
@@ -11,19 +11,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+*/
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-
+/*
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.Cookie;
+*/
 
 @WebMvcTest(TableController.class)
 public class TableControllerTests {
@@ -33,6 +35,6 @@ public class TableControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
-    
+
     
 }
--- src/test/java/group9/group9/TableModelTests.java
+++ src/test/java/group9/group9/TableModelTests.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.reactive.context.GenericReactiveWebApplicationContext;
+//import org.springframework.boot.web.reactive.context.GenericReactiveWebApplicationContext;
 
 @SpringBootTest
 public class TableModelTests {
